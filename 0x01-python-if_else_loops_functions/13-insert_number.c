2b1ade0df37ba07f662b54a30ef4a699b2150dac
evie-8 <neouma29@gmail.com>
2023-05-09 23:00:53 +0300

    insert in sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * insert_node - insert node
+ * @head: pointer
+ * @number: where to insert
+ * Return: new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *ptr;
+	int i;
+
+	ptr = *head;
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->n >= number)
+		{
+			new = insert_nodeint_at_index(head, i, number);
+			return (new);
+		}
+		else
+			ptr = ptr->next;
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * insert_nodeint_at_index - inset node at specified index
+ * @head: pointer to a listint_t pointer
+ * @idx: node position
+ * @n: data
+ * Return: node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *p, *ptr;
+	unsigned int x;
+
+	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+		return (NULL);
+	p->n = n;
+	p->next = NULL;
+	ptr = *head;
+
+	if (idx == 0)
+	{
+		p->next = ptr;
+		*head = p;
+		return (p);
+	}
+	x = 0;
+	while (x < idx - 1)
+	{
+		if (ptr == NULL || ptr->next == NULL)
+			return (NULL);
+		ptr = ptr->next;
+		x++;
+	}
+	p->next = ptr->next;
+	ptr->next = p;
+	return (p);
+}
