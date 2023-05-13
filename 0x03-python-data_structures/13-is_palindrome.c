ac5ec0d9705bbb493f389a1a5828497053734677
evie-8 <neouma29@gmail.com>
2023-05-13 17:36:25 +0300

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * is_palindrome - list is same when reversed
+ * @head: holds address of first node
+ * Return: 1 if palindrome 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr, *p;
+
+	ptr = *head;
+	p = reverse(head);
+	while (ptr != NULL && p != NULL)
+	{
+		if (ptr->n != p->n)
+			return (0);
+		ptr = ptr->next;
+		p = p->next;
+	}
+	return (1);
+}
+
+/**
+ * reverse - reverse linked list
+ * @head: contains head node address
+ * Return: node
+ */
+listint_t *reverse(listint_t **head)
+{
+	listint_t *before, *next, *current;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	before = NULL;
+	next = *head;
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = before;
+		before = current;
+		current = next;
+	}
+	*head = before;
+	return (*head);
+}
