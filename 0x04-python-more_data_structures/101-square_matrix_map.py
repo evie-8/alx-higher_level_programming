dd96eb13af36a5216305222401d968cb60a4588a
evie-8 <neouma29@gmail.com>
2023-05-18 00:18:39 +0300

    no loops

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new = list(map(lambda i: list(map(lambda x: x ** 2, i)), matrix))
-    return new
+    return list(map(lambda i: list(map(lambda x: x ** 2, i)), matrix))
