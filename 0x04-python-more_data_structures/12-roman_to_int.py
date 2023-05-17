99e1b88480160712842891c7d093fcc3815c6c19
evie-8 <neouma29@gmail.com>
2023-05-17 23:53:55 +0300

    convert to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string \
-       is None or roman_string == {}:
+    if not isinstance(roman_string, str) or roman_string is None:
         return None
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ans = 0
