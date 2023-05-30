28bb8ffa941665128ad62567f3758ff5d9bf632b
evie-8 <neouma29@gmail.com>
2023-05-30 17:38:29 +0300

    initializing object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Creating a class."""
+
+
+class Square:
+    """Creating class called Square.
+    """
+    def __init__(self, size):
+        """Describing attributes of square.
+        Note:
+            do not include `self` parameter in the `Args` section
+        Args:
+            size: dimensions of square
+        """
+        self.__size = size
