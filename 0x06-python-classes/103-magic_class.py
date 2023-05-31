b8f37b89dfe54e3e522e7173029bf66b20378298
evie-8 <neouma29@gmail.com>
2023-05-31 20:53:33 +0300

    transformimng byte code to source code,

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""creating magic class.
+"""
+
+
+class MagicClass:
+    """class called magic.
+    """
+    def __init__(self, radius=0):
+        """initialize object.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """area.
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """circumference.
+        """
+        return 2 * math.pi * self.__radius
