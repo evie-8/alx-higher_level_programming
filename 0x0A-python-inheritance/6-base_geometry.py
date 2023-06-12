5873dd33859a50d560a98b0b905ae36c284fb8f5
evie-8 <neouma29@gmail.com>
2023-06-12 17:41:44 +0300

    raising an exception if area is not claculated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""empty module"""
+
+
+class BaseGeometry:
+    """ empty class."""
+    def area(self):
+        """empty"""
+        raise Exception("area() is not implemented")
