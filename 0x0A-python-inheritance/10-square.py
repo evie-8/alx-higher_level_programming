7e41a70106620316e6298fd8bac7f35ae509621e
evie-8 <neouma29@gmail.com>
2023-06-12 19:27:15 +0300

    found error worn shebang

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythnon3
+#!/usr/bin/python3
 """modules on sqaures"""
 
 
@@ -7,13 +7,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """class square inheriting from Rectangle class"""
-
     def __init__(self, size):
         """construtor that creates instances of sqaure"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """area of a square"""
-        return self.__size * self.__size
