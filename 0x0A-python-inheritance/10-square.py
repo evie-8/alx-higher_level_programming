8b42854b9419e271808d513dd7ceaac517694612
evie-8 <neouma29@gmail.com>
2023-06-12 19:07:19 +0300

    creating square which inherits from a rectangle class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,4 +9,6 @@ class Square(Rectangle):
     """class square"""
     def __init__(self, size):
         """construtor"""
+        self.integer_validator("size", size)
         Rectangle.__init__(self, size, size)
+        self.__size = size
