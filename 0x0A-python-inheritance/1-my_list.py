3b433a1a6cdb5a132e97d9dd17805916d1d87fda
evie-8 <neouma29@gmail.com>
2023-06-12 17:07:17 +0300

    adding test for empty list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
     """class that inherits the class list"""
-    def __init__(self):
-        """creating object / instance."""
-        pass
-
     def print_sorted(self):
         """sorts list."""
         list_copy = list.copy(self)
