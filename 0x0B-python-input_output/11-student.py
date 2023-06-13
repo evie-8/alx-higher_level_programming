58a87e1dcc1f2c2ed63514b5f7dc9d3a128f7118
evie-8 <neouma29@gmail.com>
2023-06-13 23:07:23 +0300

    using json file to change atttributes of an object

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""""student class"""
+
+
+class Student:
+    """creating class called student"""
+    def __init__(self, first_name, last_name, age):
+        """creating an instance of class student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """changes class instance to json representation"""
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return {ks: getattr(self, ks) for ks in attrs if hasattr(self, ks)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """that replaces all attributes of the Student instance"""
+        for ks, vs in json.items():
+            setattr(self, ks, vs)
