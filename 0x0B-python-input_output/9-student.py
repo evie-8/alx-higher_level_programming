450dea8134c4c19dbf6bb9e6ba284e150991fa31
evie-8 <neouma29@gmail.com>
2023-06-13 22:42:31 +0300

    creating class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""student class"""
+
+
+class Student:
+    """creating class called student"""
+    def __init__(self, first_name, last_name, age):
+        """creating an instance of class student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """changes class instance to json representation"""
+        return self.__dict__
