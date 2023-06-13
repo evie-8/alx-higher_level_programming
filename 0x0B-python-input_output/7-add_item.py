812f756e21da15be1189c517276f8c5f794a338d
evie-8 <neouma29@gmail.com>
2023-06-13 22:22:25 +0300

    adding items

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module to add, save and load"""
+
+
+from sys import argv
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+for i in argv[1:]:
+    my_list.append(i)
+save_to_json_file(my_list, "add_item.json")
