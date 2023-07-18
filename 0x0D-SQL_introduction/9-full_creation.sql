f2994083584c303fbbb8b5b18a8ea6c7a72f5ac7
evie-8 <neouma29@gmail.com>
2023-07-18 21:06:20 +0300

    corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,10 @@
 -- creating table and adding content
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
-VALUES
-(1, 'John', 10),
-(2, 'ALex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+VALUES(1, 'John', 10);
+INSERT INTO second_table(id, name, score)
+VALUES(2, 'ALex', 3);
+INSERT INTO second_table(id, name, score)
+VALUES(3, 'Bob', 14);
+INSERT INTO second_table(id, name, score)
+VALUES(4, 'George', 8);
