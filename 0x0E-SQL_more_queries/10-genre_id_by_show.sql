ae657acd521b11444cfd173c35e7e64f19df7d4d
evie-8 <neouma29@gmail.com>
2023-08-18 12:32:30 +0300

    adding a README file and task 0

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- using join
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
