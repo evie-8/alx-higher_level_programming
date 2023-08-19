a521b814ac70abe789c35e21b5bd9fdfdef25559
evie-8 <neouma29@gmail.com>
2023-08-19 20:29:10 +0300

    corrections Using map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = [];
-let i = 0;
-for (i = 0; i < list.length; i++) {
-  newList[i] = list[i] * i;
-}
+const newList = list.map((items, index) => items * index);
 console.log(list);
 console.log(newList);
