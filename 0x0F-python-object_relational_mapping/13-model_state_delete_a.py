b375ad2d4299c7d344b176a81eddb81b0ffd3eeb
evie-8 <neouma29@gmail.com>
2023-08-22 00:53:58 +0300

    deleting states that contain a letter A

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    s = session.query(State).filter(State.name.ilike('%a%')).all()
+    s = session.query(State).filter(State.name.like('%a%')).all()
     for states in s:
         session.delete(states)
     # session.commit()
