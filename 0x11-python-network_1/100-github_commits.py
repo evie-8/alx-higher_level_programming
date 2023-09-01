40c3503652c5eade3123e78d96bd02b5d3b0a075
evie-8 <neouma29@gmail.com>
2023-09-02 00:05:53 +0300

    commits using request

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""interview questions for holberton"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    data = requests.get(url)
+    commits = data.json()
+
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=": ")
+        print(commit.get('commit').get('author').get('name'))
