9cc34aa490601d39a3b597e352ab252749ad1aa8
evie-8 <neouma29@gmail.com>
2023-09-02 00:10:51 +0300

    commits using request

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,13 @@ if __name__ == "__main__":
     from sys import argv
     import requests
 
-    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
-    data = requests.get(url)
-    commits = data.json()
+    try:
+        url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+        data = requests.get(url)
+        commits = data.json()
 
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=": ")
-        print(commit.get('commit').get('author').get('name'))
+        for commit in commits[:10]:
+            print(commit.get('sha'), end=": ")
+            print(commit.get('commit').get('author').get('name'))
+    except ValueError as e:
+        pass
