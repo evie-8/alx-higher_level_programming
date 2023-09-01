533ce3a997ebc27bac4360a971565f35eaf973fc
evie-8 <neouma29@gmail.com>
2023-09-01 22:44:46 +0300

    corrections

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         with request.urlopen(url) as message:
             print(message.read().decode("UTF-8"))
     except error.HTTPError as errors:
-        print("Error code:", error.code)
+        print("Error code:", errors.code)
