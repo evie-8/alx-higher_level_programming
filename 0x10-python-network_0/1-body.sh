0790ae4542d3e9b76b0befbad86f7adb1e52f0ae
evie-8 <neouma29@gmail.com>
2023-09-01 17:01:45 +0300

    a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
    Display only body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
- if curl -s -I "$1" | grep -q "200 OK"; then curl -s --get "$1"; fi
+ if curl -s -I -L "$1" | grep -q "200 OK"; then curl -s -L --get "$1"; fi
