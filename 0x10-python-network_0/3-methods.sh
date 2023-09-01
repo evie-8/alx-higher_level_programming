e10a486a7b4d0e2e2e5358178c72363fafc0a1ea
evie-8 <neouma29@gmail.com>
2023-09-01 18:11:11 +0300

    a Bash script that takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I -L -X OPTIONS "$1" | sed -n '/Allow:/ p' | cut -d ":" -f 2
+curl -s -I -L -X OPTIONS "$1" | sed -n '/Allow:/ p' | cut -d ":" -f 2 | sed 's/^[[:space:]]*//'
